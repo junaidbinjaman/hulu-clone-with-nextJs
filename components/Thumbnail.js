ba956a4aa900da2799120cb0fafc94729c356cd9
iMac <imac@jwolts-iMac.local>
2021-11-13 13:43:51 +0600

    complet hulu clone with next js

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,12 +1,13 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from 'next/image';
-import React, { forwardRef } from 'react';
+import { useRef } from 'react';
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = ({ result }, ) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/"
+  const reference = useRef()
   
   return (
-    <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div ref={reference}  className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image 
         layout="responsive"
         src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}
@@ -27,6 +28,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       </div>
     </div>
   );
-})
+};
 
 export default Thumbnail;
